4a831b9821c9cd07c66a3b59258122f763bcff94
Tihomir Vitanov <vitanovtd@gmail.com>
2024-07-31 19:55:37 +0300

    added additionally functionalities

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber = null;
+let secondNumber = null;
+let currentOperator = null;
+
 function add(a, b) {
   return a + b;
 }
@@ -16,3 +20,55 @@ function divide(a, b) {
   }
   return a / b;
 }
+
+function percentage(a) {
+  return a / 100;
+}
+
+function square(a) {
+  return a * a;
+}
+
+function squareRoot(a) {
+  return Math.sqrt(a);
+}
+
+function reciprocal(a) {
+  if (a === 0) {
+    return "error: division by zero";
+  }
+  return 1 / a;
+}
+
+function backSpace(numberStr) {
+  return numberStr.slice(0, -1);
+}
+
+function operate(operator, a, b = null) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    case "%":
+      return percentage(a);
+    case "x²":
+      return square(a);
+    case "√":
+      return squareRoot(a);
+    case "1/x":
+      return reciprocal(a);
+    default:
+      return null;
+  }
+}
+
+// console.log(add(3, 5));
+// console.log(subtract(10, 4));
+// console.log(multiply(7, 6));
+// console.log(divide(8, 2));
+// console.log(divide(8, 0));
